0b4deb46905b2d756d89a74fab7894c42d694c96
Hiago Souza <hiagofss98@gmail.com>
2019-08-08 19:45:59 -0300

    Create Routes and organize files

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import './Login.css';
 
 import logo from '../../assets/logo.svg';
 
 export default function Login() {
-  return <img src={logo} alt="" />;
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Tindev" />
+        <input placeholder="Digite seu usuario no GitHub" />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
