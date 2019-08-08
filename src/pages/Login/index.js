d109d34bc7465b7fb79acfc6d3ef914b959bd2d6
Hiago Souza <hiagofss98@gmail.com>
2019-08-08 19:15:59 -0300

    Create component Login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+import logo from '../../assets/logo.svg';
+
+export default function Login() {
+  return <img src={logo} alt="" />;
+}
